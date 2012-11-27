dc5ff008f62fae41f9dceff0b497b0ab283ee98e
Daniel Hahn <dha@betterplace.org>
2012-11-27 18:40:05 +0100

    Fiddling with jasmine-ruby

---
+++ spec/javascripts/BlogPostSpec.js
@@ -0,0 +1,11 @@
+describe("GS.model.BlogPost", function() {
+
+  beforeEach(function () {
+    blogpost = Ext.create('GS.model.BlogPost');
+  });
+
+  it("be a true Foo", function() {
+    expect(true).toBe(true);
+  });
+
+});
